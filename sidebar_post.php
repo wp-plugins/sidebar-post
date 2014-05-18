51bf9228fa9131228a7bb9e1437a1c762d35a1d1
ishimwe <ishimwe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 05:46:47 +0000

    Updating from 1.0.1 to 1.1.0 with tons of features
    git-svn-id: https://plugins.svn.wordpress.org/sidebar-post/tags/1.1.0@916390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sidebar_post.php
+++ sidebar_post.php
@@ -3,7 +3,7 @@
 Plugin Name: Sidebar Posting
 Plugin URI: http://www.janvierdesigns.com
 Description: This plugin places a posting form in the sidebar
-Version: 1.1.0
+Version: 1.2
 Author: Janvier M @ JanvierDesigns
 Author URI: http://www.janvierdesigns.com 
 
@@ -30,8 +30,8 @@ function spost_loaded() {
 add_action( 'plugins_loaded', 'spost_loaded', 20 );
 global $shortname;
 $shortname = "spost_";
-define('SPOST_VERSION','1.1.0');
-define('SPOST_DATABASE_VERSION','1.1.0');
+define('SPOST_VERSION','1.2');
+define('SPOST_DATABASE_VERSION','1.2');
 define('SPOST_BUILD','05172014');
 define('SOFTWARE_NAME','Sidebar Post');
 define( 'SPOST_PLUGIN_DIR', WP_PLUGIN_DIR . '/sidebar-post' );
@@ -57,7 +57,9 @@ function curPageName() {
  function spost_script_enqueue_js_css() {
 
 	  wp_register_script( 'spost-validate', SPOST_PLUGIN_URL . '/js/validate.js');
+	  wp_register_script( 'spost', SPOST_PLUGIN_URL . '/js/spost.js');
 	  wp_enqueue_script( 'spost-validate' );
+	  wp_enqueue_script( 'spost' );
   	wp_register_style( 'spost',  SPOST_PLUGIN_URL . '/css/spost.css',  array(), '', 'screen' );
     wp_enqueue_style( 'spost' );	
 	 
@@ -77,32 +79,33 @@ function sidebar_post_widget() { if ( is_user_logged_in() ){
 				$display_name = $current_user->display_name . "\n";
 			?>
 <div id="wp_sidebarpost_main">
-	<form action="" method="post" onsubmit="return validate_form(this)">
+	<form action="" method="post" onsubmit="return validate_form(this)" id="SidebarPost">
     <input type="hidden" name="submit-post" value="yes" />
+    <input type="hidden" name="current_user_id" id="current_user_id" value="<?php echo get_current_user_id(); ?>" />
+    <input type="hidden" name="AjaxUrl" id="AjaxUrl" value="<?php bloginfo('home') ?>/wp-admin/admin-ajax.php" />
+    
 	  <p>
 	    <label><strong><?php _e('Yout Post Title','spost'); ?></strong><br>
-	    <input name="title" type="text" style="width: 90%; background:#EFEFEF" value="">
+	    <input name="title" id="spost_title" type="text" style="width: 90%; background:#EFEFEF" value="">
 	    </label>
 </p>
 	  <p>
 	    <label> <strong><?php _e('Content of the post','spost'); ?> </strong><br>
-	    <textarea name="content" style="width: 90%; background: #EFEFEF"></textarea>
+	    <textarea id="spost_content" name="spost_content" style="width: 90%; background: #EFEFEF"></textarea>
 		</label>
 	    <br>
 		<label><strong><?php _e('Category','spost'); ?></strong>
 			<?php
-				$select = wp_dropdown_categories('show_option_none=Select a category&show_count=1&orderby=name&echo=0');
+				$select = wp_dropdown_categories('show_option_none=Select a category&show_count=1&orderby=name&echo=0&id=spost_category');
 				$select = preg_replace("#<select([^>]*)>#", "<select$1 >", $select);
 				echo $select;
-			?>
-				
+			?>	
 		</label>
 		<input type="hidden" name="owner" value="<?php echo $user_id;?>"  />
-	    <input type="submit" name="send_post" value="Send for review" style="width: 150px; height: 40px; background:#EFEFEF" />
+	    <input type="submit" name="send_post"  id="send_post" value="Send for review" style="width: 150px; height: 40px; background:#EFEFEF" />
         </p>	
 	</form>
-
-
+    <div id="spostMessage"></div>
 </div>
 <?php }else{ 
 $site_home = get_option('home');
@@ -201,4 +204,43 @@ function sidebar_posting_menu() {
 	add_options_page('SidebarPosting', 'Sidebar Post', 'manage_options', 'sidebar-posting', 'sidebar_post_fx');
 }
 function sidebar_post_fx(){}
+
+function create_spost_ajax(){
+	$cat_= $_POST['spost_category'];
+	$owner = $_POST['current_user_id'];
+	$title = $_POST['spost_title'];
+	$content = $_POST['spost_content'];
+	  
+	   // Create post object
+  $sPost = array();
+  $sPost['post_title'] = $title;
+  $sPost['post_content'] = $content;
+  $sPost['post_status'] = 'draft';
+  $sPost['post_author'] = $owner;
+  $sPost['post_type'] = 'post';
+  $sPost['post_category'] = array($cat_);
+  
+  if(is_user_logged_in()){
+// Insert the post into the database
+	$successful_post = wp_insert_post($sPost);	
+	  if($successful_post){
+		  _e('Thank you for submitting your post. It will be reviewed and once approved, published','');
+$admin_email = array(get_option('admin_email'));
+
+add_filter('wp_mail_content_type',create_function('', 'return "text/html"; '));
+wp_mail($admin_email, __('New Post created on website','spost'), '<p>A New post created. '.'<p>Please moderate...<p><a href="'.get_option('home').'/wp-admin/post.php?post='.$successful_post.'&action=edit">'.get_option('home').'/wp-admin/post.php?post='.$successful_post.'&action=edit</a>');
+
+// Reset content-type to avoid conflicts -- http://core.trac.wordpress.org/ticket/23578
+remove_filter( 'wp_mail_content_type', 'set_html_content_type' );
+
+		  }
+	 }
+	  
+
+  
+	die();
+	
+}
+add_action('wp_ajax_spostPublish', 'create_spost_ajax');
+add_action('wp_ajax_nopriv_spostPublish', 'create_spost_ajax');//for users that are not logged in.
 ?>
