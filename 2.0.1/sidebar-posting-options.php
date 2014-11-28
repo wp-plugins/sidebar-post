ec006efb223fa9afa983bfa32f06591320bcf0ba
ishimwe <ishimwe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 19:22:50 +0000

    Updated to recent version for  WP 4.0.1  For  tag 2.0.1
    git-svn-id: https://plugins.svn.wordpress.org/sidebar-post/tags@1034679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 2.0.1/sidebar-posting-options.php
@@ -0,0 +1,7 @@
+<?php 
+### Variables Variables Variables
+$base_name = plugin_basename('sidebar-posting/sidebar-posting-options.php');
+$base_page = 'admin.php?page='.$base_name;
+$mode = trim($_GET['mode']);
+$pagenavi_settings = array('pagenavi_options');
+?>
